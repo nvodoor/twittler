c4e07bae7988a2b61ba330f21b942d174bc48fb7
nvodoor <nvodoor@gmail.com>
2017-01-26 18:55:29 -0800

    Add files via upload

--- data_generator.js
+++ data_generator.js
@@ -58,6 +58,7 @@ scheduleNextTweet();
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
+
 var writeTweet = function(message){
   if(!visitor){
     throw new Error('set the global visitor property!');
@@ -65,5 +66,6 @@ var writeTweet = function(message){
   var tweet = {};
   tweet.user = visitor;
   tweet.message = message;
+  tweet.created_at = new Date();
   addTweet(tweet);
 };
